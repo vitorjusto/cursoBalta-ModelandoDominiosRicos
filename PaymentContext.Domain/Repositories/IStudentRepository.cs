b1a2636d0589c9b7917db50699f71fc14cfa911e
Vitor Justo <vitor.justo@inovafarma.com.br>
2022-10-07 17:46:45 -0300

    repository pattern

---
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        bool DocumentExist(string document);
+        bool EmailExist(string email);
+        void CreateSubscription(Student student);
+    }
+}
